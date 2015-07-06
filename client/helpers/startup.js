3b69887fe5a41cc0912050514a7265af9517d4ff
Ramón Serrano <ramon.calle.88@gmail.com>
2015-07-05 19:38:06 -0430

    Updated. Added customizations.

--- client/helpers/startup.js
+++ client/helpers/startup.js
@@ -1,4 +1,13 @@
 // Apply AdminLTE skin
-Meteor.startup(function() {
-   $('body').addClass('skin-blue');
+Meteor.startup(function() {	
+	if (typeof AdminConfig === 'undefined') {
+		AdminConfig = {
+			skin: 'skin-blue'
+		};
+	}
+
+	if (AdminConfig.skin !== 'undefined' && typeof AdminConfig.skin === 'string')
+		$('body').addClass(AdminConfig.skin);
+	else
+		$('body').addClass('skin-blue');
 });
